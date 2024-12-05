dc0d205856224b959193ba638124ad4b036bb0d3
Mickael Dias <mickaeldias00@gmail.com>
2024-12-05 03:09:38 -0400

    [feat]: [Tela de Clientes] [desc]: [Criação da tabela de clientes e do container de interações (não funcional)]

--- src/app/pages/cliente/cliente.component.ts
+++ src/app/pages/cliente/cliente.component.ts
@@ -2,17 +2,37 @@ import { Component } from '@angular/core';
 import { ClienteService } from '../../services/cliente.service';
 import { HttpClientModule } from '@angular/common/http';
 import { ICliente } from '../../interfaces/ICliente';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-cliente',
   standalone: true,
-  imports: [HttpClientModule],
+  imports: [
+    HttpClientModule,
+    IconFieldModule,
+    InputIconModule,
+    InputTextModule,
+    ButtonModule,
+    MultiSelectModule,
+    TableModule
+  ],
   templateUrl: './cliente.component.html',
   styleUrl: './cliente.component.scss'
 })
 export class ClienteComponent {
 
   clientes: ICliente[] = [];
+  clientesSelecionados: ICliente[] = []
+  niveisConfiabilidade = [
+    {id: 1, desc: 'Baixa'},
+    {id: 2, desc: 'Média'},
+    {id: 3, desc: 'Alta'}
+  ];
 
   constructor(private clienteService: ClienteService) { }
 
